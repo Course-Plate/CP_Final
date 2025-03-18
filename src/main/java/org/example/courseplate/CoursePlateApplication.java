98f181ef133941af698f5694b5498b990b59d113
KimMundo01 <801sml@gmail.com>
2025-03-18 12:23:29 +0900

    backend기본틀

---
+++ src/main/java/org/example/courseplate/CoursePlateApplication.java
@@ -0,0 +1,13 @@
+package org.example.courseplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePlateApplication.class, args);
+    }
+
+}
