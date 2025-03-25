ae682cec79a850de2b7ccc01e61d2a8c39dbf29d
KimMundo01 <801sml@gmail.com>
2025-03-25 15:07:34 +0900

    SecurityConfig places 추가

--- src/main/java/org/example/courseplate/config/SecurityConfig.java
+++ src/main/java/org/example/courseplate/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                                 .requestMatchers( "/").permitAll()
                                 .requestMatchers( "/users/**").permitAll()
                                 .requestMatchers( "/auth/**").permitAll()
+                                .requestMatchers( "/places/**").permitAll()
                                 .anyRequest().authenticated()
                 );
         return  http.build();
