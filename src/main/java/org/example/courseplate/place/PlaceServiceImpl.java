51bc77e6bcfd4f8b8097dc0c89d99dd72cb627da
KimMundo1 <801sml@gmail.com>
2025-05-05 15:13:31 +0900

    place 주석 추가

--- src/main/java/org/example/courseplate/place/PlaceServiceImpl.java
+++ src/main/java/org/example/courseplate/place/PlaceServiceImpl.java
@@ -16,27 +16,32 @@ public class PlaceServiceImpl implements PlaceService {
         this.placeRepository = placeRepository;
     }
 
+    // 장소 추가
     @Override
     public Place add(Place place) {
         return placeRepository.save(place);
     }
 
+    // 장소 이름으로 주소 반환
     @Override
     public String getPlaceByPlaceName(String placeName) {
         Place place = placeRepository.findByPlaceName(placeName);
         return place != null ? place.getAddress() : null;
     }
 
+    // 전체 장소 목록 반환
     @Override
     public List<Place> getAllPlaces() {
         return placeRepository.findAll();
     }
 
+    // placeId로 장소 정보 반환
     @Override
     public Place getPlaceByPlaceId(String placeId) {
         return placeRepository.findByPlaceId(placeId);
     }
 
+    // ID로 장소 정보 수정
     @Override
     public Place updatePlace(String id, Place updatedPlace) {
         Optional<Place> optionalPlace = placeRepository.findById(id);
@@ -53,6 +58,7 @@ public class PlaceServiceImpl implements PlaceService {
         throw new IllegalArgumentException("Place not found with id: " + id);
     }
 
+    // ID로 장소 삭제
     @Override
     public void deletePlace(String id) {
         placeRepository.deleteById(id);
