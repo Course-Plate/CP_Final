c75b5157ac06e0e98ab1c7fdf2cb2a5df486542d
JY <duawodud12@naver.com>
2025-06-04 00:37:46 +0900

    불필요한 파일 삭제

--- CP_Frontend/context/AppContext.js
+++ CP_Frontend/context/AppContext.js
@@ -1,17 +1,11 @@
 // context/AppContext.js
 import React from 'react';
 import { FontProvider } from './FontContext';
-import { PreferencesProvider } from './PreferencesContext';
-import {PlacesProvider} from "./PlacesContext";
 
 export function AppProviders({ children }) {
     return (
         <FontProvider>
-            <PreferencesProvider>
-                <PlacesProvider>
-                    {children}
-                </PlacesProvider>
-            </PreferencesProvider>
+            {children}
         </FontProvider>
     );
 }
